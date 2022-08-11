3fd32bba0ff433cfe93761fc5cf8bdb271b2713d
labradorescence <orangenewyorkcreativeagency@gmail.com>
2022-08-11 16:03:17 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const init = () => {
-  
+  fetch("http://localhost:3000/movies")
+  .then(resp => resp.json())
+  .then(data => console.log(data))
 }
 
 document.addEventListener('DOMContentLoaded', init);
+document.querySelectorAll("#searchByID")
+
+
+const form = document.querySelector('form')
+const input = document.querySelector('#searchByID')
+const submitbtn = document.querySelector('form>input')
+
+form.addEventListener('submit', submitFunc)
+
+const submitFunc = () => {
+    preventDefault()
+    defaultPrevent()
+    console.log("submitted")
+}
